eb28e68ff5c821363fed3e31d5d80c52198648b5
liut <liut@phoenixtv.com>
2021-09-23 14:38:20 +0800

    more stats

--- src/lib/kedge/StatsBar.svelte
+++ src/lib/kedge/StatsBar.svelte
@@ -12,8 +12,10 @@
 <p class="d-flex">
 {#if stats}
   <Chip>task count: {stats.taskCount || 0}</Chip>
-  <Chip>R: {utils.formatBytes(stats.rateRecv || 0)}</Chip>
-  <Chip>S: {utils.formatBytes(stats.rateSent || 0)}</Chip>
+  <Chip><span title="num peers connected">PC: {stats.numPeersConnected || 0}</span></Chip>
+  <Chip><span title="num peers half open">PHO: {stats.numPeersHalfOpen || 0}</span></Chip>
+  <Chip><span title="download rate">R: {utils.formatBytes(stats.rateRecv || 0)}</span></Chip>
+  <Chip><span title="upload rate">S: {utils.formatBytes(stats.rateSent || 0)}</span></Chip>
 
   <Badge class="primary-color" bordered value={stats.numDownloading || 0} offsetX={16} offsetY={16}>
     <Chip><span class="icon"><FaDownload /></span></Chip>
